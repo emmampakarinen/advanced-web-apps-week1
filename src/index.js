6ea74714f8eed97a3806cfd82cd8d1615fa65c5f
CodeSandbox <support@codesandbox.io>
2023-10-31 14:49:17 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -8,17 +8,35 @@ if (document.readyState !== "loading") {
   });
 }
 
-function initializeCode() {
-  //document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+async function initializeCode() {
+
   let container = document.getElementById("container");
-  for (let i = 0; i < 5; i++) {
+  let breedList = ["terrier/westhighland", 'labrador', 'terrier/scottish', 'retriever/golden', 'poodle/standard', 'corgi', 'terrier/norwich']
+  let wikiList = ['West highland white terrier', 'Labrador', 'Scottish terrier', 'Golden retriever', 'Standard poodle', 'Corgi', 'Norwich terrier']
+
+  for (let i in breedList) {
+    let breed = breedList[i]
+    let url1 = "https://dog.ceo/api/breed/" + breed +  "/images/random"
+    let url2 = "https://en.wikipedia.org/api/rest_v1/page/summary/" + encodeURIComponent(wikiList[i])
+
+
+    let promise = await fetch(url1)
+    let data = await promise.json()
+
+    let wikiPromise = await fetch(url2)
+    let wikiData = await wikiPromise.json()
+
+    let dogUrl = data.message
+
     let item = document.createElement("div");
     item.classList.add("wiki-item");
-    container.appendChild(item);
+    container.appendChild(item); 
 
     let header = document.createElement("h1");
     header.classList.add("wiki-header");
-    header.innerText = "Breed X";
+
+
+    header.innerText = wikiList[i];
     item.appendChild(header);
 
     let contentContainer = document.createElement("div");
@@ -27,7 +45,7 @@ function initializeCode() {
 
     let content = document.createElement("p");
     content.classList.add("wiki-text");
-    content.innerHTML = "Some text about this breed";
+    content.innerHTML = wikiData.extract;
     contentContainer.appendChild(content);
 
     let imgContainer = document.createElement("div");
@@ -35,6 +53,7 @@ function initializeCode() {
     contentContainer.appendChild(imgContainer);
 
     let img = document.createElement("img");
+    img.src = dogUrl
     img.classList.add("wiki-img");
     imgContainer.appendChild(img);
   }
